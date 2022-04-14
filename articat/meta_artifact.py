ea5a037a7c2808cd4437dafde2282db6f0723387
Rafal Wojdyla <ravwojdyla@gmail.com>
2022-04-14 19:09:07 +0200

    Small follow ups from the meta artifact

--- articat/meta_artifact.py
+++ articat/meta_artifact.py
@@ -8,11 +8,17 @@ class MetaArtifact(Artifact):
     """Metadata only Artifact. Primary use case is to aggregate information about multiple artifacts."""
 
     def deps(
-        self, fetch: bool = False, catalog: Optional[type[Catalog]] = None
+        self,
+        fetch: bool = False,
+        catalog: Optional[type[Catalog]] = None,
+        dep_model_cls: type[Artifact] = Artifact,
     ) -> list[Artifact]:
-        """Returns list of dependencies of this MarkerArtifact"""
+        """
+        Returns list of dependencies of this MetaArtifact, use `dep_model_cls` if
+        you want all deps parsed as a specific Artifact class (e.g. FSArtifact).
+        """
         deps = [
-            Artifact(id=d["id"], partition=d["partition"], version=d["version"])
+            dep_model_cls(id=d["id"], partition=d["partition"], version=d["version"])
             for d in self.metadata.arbitrary["deps"]
         ]
         return deps if not fetch else [d.fetch(catalog=catalog) for d in deps]
@@ -21,5 +27,5 @@ class MetaArtifact(Artifact):
         """MetaArtifact doesn't really have a proper URL website to open"""
         return ""
 
-    def build(self) -> "Artifact":
+    def build(self) -> "MetaArtifact":
         return self
