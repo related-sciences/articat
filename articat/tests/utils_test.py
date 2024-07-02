87bf43361be13d6ee6660778e72bc57f4eda32b8
Dan Frank <df+github@related.vc>
2024-07-02 14:39:17 -0700

    Fix whitespace error

--- articat/tests/utils_test.py
+++ articat/tests/utils_test.py
@@ -15,6 +15,7 @@ from articat.utils.path_utils import cwd
 from articat.utils.typing import PathType
 from articat.utils.utils import download_artifact, dummy_unsafe_cache, get_repo_and_hash
 
+
 def get_source_path_that_looks_like_path_from_catalog() -> Path:
     p = Path(
         tempfile.mkdtemp(dir=TestFSArtifact.config().fs_prod_prefix(), suffix="__ID__")
